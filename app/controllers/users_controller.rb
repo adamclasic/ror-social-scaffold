a87cb944cb135925f6c91da87292ddd559c6a48f
Brittany Blake <brittany.blake49@gmail.com>
2020-06-25 16:20:32 +0200

    Fix friend reject button

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
 
   def index
     @users = User.all
-    @pending_requests = current_user.friend_requests
+    @pending_requests = current_user.pending_friends
   end
 
   def show
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,24 +10,24 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :friendships, dependent: :destroy
-  has_many :inverse_friendships, :class_name => "Friendship", :foreign_key => "friend_id"
+  has_many :inverse_friendships, class_name: 'Friendship', foreign_key: 'friend_id'
 
   def friends
-    friends_array = friendships.map{|friendship| friendship.friend if friendship.confirmed}
-    friends_array += inverse_friendships.map{|friendship| friendship.user if friendship.confirmed}
+    friends_array = friendships.map { |friendship| friendship.friend if friendship.confirmed }
+    # friends_array += inverse_friendships.map { |friendship| friendship.user if friendship.confirmed }
     friends_array.compact
   end
 
   # Users who have yet to confirme friend requests
   def pending_friends
-    friendships.map{|friendship| friendship.friend if !friendship.confirmed}.compact
+    friendships.map { |friendship| friendship.friend unless friendship.confirmed }.compact
   end
 
   # Users who have requested to be friends
   def friend_requests(user_id)
-      #inverse_friendships.map{|friendship| friendship.user if !friendship.confirmed}.compact
-     friendship = friendships.where(friend_id: user_id).first
-     true if friendship && friendship.confirmed == false
+    # inverse_friendships.map{|friendship| friendship.user if !friendship.confirmed}.compact
+    friendship = friendships.where(friend_id: user_id).first
+    true if friendship && friendship.confirmed == false
   end
 
   def receive_invitation(user_id)
@@ -35,7 +35,7 @@ class User < ApplicationRecord
     true if friendship && friendship.confirmed == false
   end
 
-  #Send friend requests
+  # Send friend requests
   def send_request(user_id)
     @friendship = Friendship.new(user_id: id, friend_id: user_id)
     @friendship.confirmed = false
@@ -56,6 +56,7 @@ class User < ApplicationRecord
   def reject_friend(user)
     # friendship = inverse_friendships.find{|friendship| friendship.user == user}
     friendship = inverse_friendships.where(user_id: user).first
+     friendship.confirmed = false
     friendship.destroy
   end
 

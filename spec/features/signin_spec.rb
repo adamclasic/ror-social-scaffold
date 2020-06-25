e743f8a2f8ea37af1cb5225a206b8686108c6f21
Brittany Blake <brittany.blake49@gmail.com>
2020-06-25 18:15:02 +0200

    Add integration tests for sign in and sign up

---
+++ spec/features/signin_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.feature 'Sign up', type: :feature do
+  before :each do
+    User.create(email: 'jane@example.com', name: 'jane', password:'password')
+  end
+
+  scenario 'Sign in a user' do
+    visit new_user_session_path
+    fill_in 'user_email', with: 'jane@example.com'
+    fill_in 'user_password', with: 'password'
+    click_button('submit')
+    expect(page).to have_content 'Signed in successfully'
+  end
+end
